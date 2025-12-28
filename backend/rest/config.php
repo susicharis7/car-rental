4fe7924873fc90c7c60f4b74f3a5043a01bdb212
susicharis7 <susicharis99@gmail.com>
2025-12-29 00:47:02 +0100

    Updated config.php for deployment

--- backend/rest/config.php
+++ backend/rest/config.php
@@ -1,34 +1,42 @@
 <?php
 
 // Set the reporting
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
 class Config {
+    
     public static function DB_NAME() {
-        return 'car-rental';
+        return self::get_env("DB_NAME", "car-rental");
     }
 
     public static function DB_PORT() {
-        return 3306;
+        return self::get_env("DB_PORT", 3306);
     }
 
     public static function DB_USER() {
-        return 'root';
+        return self::get_env("DB_USER", "root");
     }
 
     public static function DB_PASSWORD() {
-        return 'harkeking333'; 
+        return self::get_env("DB_PASSWORD", "harkeking333");
     }
     
     public static function DB_HOST() {
-        return 'localhost';
+        return self::get_env("DB_HOST", "localhost");
     }
 
-    // JWT
     public static function JWT_SECRET() {
-        return 'harkeking321';
+        return self::get_env("JWT_SECRET", "harkeking321");
     }
     
+    // Ova funkcija provjerava da li postoji environment varijabla
+    // Ako postoji - koristi nju (za production/deployment)
+    // Ako ne postoji - koristi default vrijednost (za lokalni razvoj)
+    public static function get_env($name, $default) {
+        return isset($_ENV[$name]) && trim($_ENV[$name]) != "" 
+            ? $_ENV[$name] 
+            : $default;
+    }
 }
